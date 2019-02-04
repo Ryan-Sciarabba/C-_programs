c67a55d6f2b42f3d8bebc9a9378451ff57dbb6e6
Ryan-Sciarabba <rhsciarabba@gmail.com>
2019-02-04 01:30:43 -0500

    Fixed for product specification

--- GHP#1/main.cpp
+++ GHP#1/main.cpp
@@ -2,9 +2,9 @@
 Purpose: To search a matrix for a user-given value
 Written by: Ryan Sciarabba
 Language: C++ (g++ target)
-Version: 1.10.11
+Version: 1.11.14
 Date of Creation: 1/28/2019
-Date of Last Revision: 1/30/2019
+Date of Last Revision: 2/4/2019
 */
 #include <iostream>
 
@@ -14,6 +14,7 @@ int main()
 {
     int a[2][2]; //2x2 matrix
     int b, //User input value
+        c = 0, //Check to see if value was found
         i, //Row increment
         j, //Column increment
         k, //Row comparator
@@ -31,18 +32,21 @@ int main()
     cout<<a[1][0]<<" "<<a[1][1]<<endl<<endl;
 
     //Search for a number
-    cout<<"Input a whole number between 1 and 4 to search the matrix for: ";
+    cout<<"Input a whole number to search the matrix for: ";
     cin>>b;
     for(i = 0; i < 2 ; i++){
         for(j = 0; j < 2 ; j++){
             if(a[i][j] == b){ //If the input number and the current number are equal store the value of its column and row
-                k = i;
-                l = j;
+                cout<< b << " was found."<<endl;
+                c = 1;
             }
         }
     }
+    if(c == 0){
+        cout<< b << " was not found."<<endl;
+    }
     //Print where the number is in the matrix
-    cout<< b <<" is in row "<< k + 1 <<" and column "<< l + 1 <<endl;
+
 
     return 0;
 }
