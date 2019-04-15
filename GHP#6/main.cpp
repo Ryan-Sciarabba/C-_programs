819a60193ceed71e32c3cc12c0ac3a0da7d4fbb3
Ryan Sciarabba <rhsciarabba@gmail.com>
2019-04-15 17:21:05 -0400

    Fixed GHP#6 by adding vectors

--- GHP#6/main.cpp
+++ GHP#6/main.cpp
@@ -2,13 +2,13 @@
 Purpose: To take in a list of stock prices to manipulate and display the data
 Written by: Ryan Sciarabba
 Language: C++ (g++ target)
-Version: 1.7.4
+Version: 2.7.4
 Date of Creation: 4/15/2019
 Date of Last Revision: 4/15/2019
 */
 #include <iostream>
 #include <algorithm>
-#include <array>
+#include <vector>
 #include <numeric>
 
 using namespace std;
@@ -20,24 +20,24 @@ int main()
 
     double price; //Input price
 
-    array<double,15> aPrice;
-    array<double,14> pDiff;
+    vector<double> vPrice; //Vector for prices
+    vector<double> pDiff(16); //Vector for price differences
 
-    //Store prices in a stack
+    //Store prices in a vector
     while(day <= 15){
         cout<<"Please input the stock price on day "<<day<<": ";
         cin>>price;
-        aPrice[day - 1] = price;
+        vPrice.push_back(price);
         day++;
     }
     cout<<endl;
 
     //Find highest and lowest trading prices
-    cout<<"Lowest Price: "<<*min_element(aPrice.begin(), aPrice.end())<<endl;
-    cout<<"Highest Price: "<<*max_element(aPrice.begin(), aPrice.end())<<endl<<endl;
+    cout<<"Lowest Price: "<<*min_element(vPrice.begin(), vPrice.end())<<endl;
+    cout<<"Highest Price: "<<*max_element(vPrice.begin(), vPrice.end())<<endl<<endl;
 
     //Find differences per day of trade prices
-    adjacent_difference(aPrice.begin(), aPrice.end(), pDiff.begin());
+    adjacent_difference(vPrice.begin(), vPrice.end(), pDiff.begin());
 
     while(diff <= 14){
         cout<<"Price difference between day "<<diff<<" and "<<diff + 1<<": "<<pDiff[diff]<<endl;
@@ -46,15 +46,16 @@ int main()
     cout<<endl;
 
     //Sort prices from low to high
-    sort(aPrice.begin(), aPrice.end());
+    sort(vPrice.begin(), vPrice.end());
 
     day = 1;
 
     cout<<"Prices from low to high: ";
     while(day <= 15){
-        cout<<aPrice[day - 1]<<" ";
+        cout<<vPrice.at(day - 1)<<" ";
         day++;
     }
+    cout<<endl;
 
     return 0;
 }
